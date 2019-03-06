487a398e41780c8742ebe409d14e8c0a287484a9
Kevinoh47 <kohok47@gmail.com>
2019-03-05 17:17:25 -0800

    implemented a stack using queues

--- index3-sq.js
+++ index3-sq.js
@@ -1,5 +1,5 @@
 'use strict';
-const {Stack, Queue} = require('./stacks-and-queues');
+const {Stack, Queue, QStack} = require('./stacks-and-queues');
 const LinkedList = require('./linked-list');
 
 let myList = new LinkedList();
@@ -10,7 +10,97 @@ console.log('\n ---\n');
 console.log ({'iterate my list': myList.iterator()});
 
 console.log('\n ---\n');
+// write a function that implements a stack using queues
+// implemented in stacks-and-queues.js
+
+let myQStack = new QStack();
+
+myQStack.push(1);
+myQStack.push(2);
+myQStack.push(3);
+myQStack.push(4);
+
+console.log({'q1Count':myQStack.q1Count, 'q1LastEnqueued': myQStack.q1LastEnqueued, 'q2Count':myQStack.q2Count, 'q2LastEnqueued': myQStack.q2LastEnqueued, 'currentQ':myQStack.currentQ});
+console.log('\n ---\n');
+console.log({'peek 4': myQStack.peek()});
+console.log({'pop 4':myQStack.pop()});
+console.log('\n ---\n');
+console.log({'q1Count':myQStack.q1Count, 'q1LastEnqueued': myQStack.q1LastEnqueued, 'q2Count':myQStack.q2Count, 'q2LastEnqueued': myQStack.q2LastEnqueued, 'currentQ':myQStack.currentQ});
+console.log('\n ---\n');
+console.log({'peek 3': myQStack.peek()});
+console.log({'pop 3':myQStack.pop()});
+console.log('\n ---\n');
+console.log({'q1Count':myQStack.q1Count, 'q1LastEnqueued': myQStack.q1LastEnqueued, 'q2Count':myQStack.q2Count, 'q2LastEnqueued': myQStack.q2LastEnqueued, 'currentQ':myQStack.currentQ});
+console.log('\n ---\n');
+console.log({'peek 2': myQStack.peek()});
+console.log({'pop 2': myQStack.pop()});
+console.log('\n ---\n');
+console.log({'q1Count':myQStack.q1Count, 'q1LastEnqueued': myQStack.q1LastEnqueued, 'q2Count':myQStack.q2Count, 'q2LastEnqueued': myQStack.q2LastEnqueued, 'currentQ':myQStack.currentQ});
+console.log('\n ---\n');
+console.log({'peek 1': myQStack.peek()});
+console.log({'pop 1': myQStack.pop()});
+console.log('\n ---\n');
+console.log({'q1Count':myQStack.q1Count, 'q1LastEnqueued': myQStack.q1LastEnqueued, 'q2Count':myQStack.q2Count, 'q2LastEnqueued': myQStack.q2LastEnqueued, 'currentQ':myQStack.currentQ});
+console.log('\n ---\n');
+console.log({'peek none': myQStack.peek()});
+console.log({'pop none': myQStack.pop()});
+console.log('\n ---\n');
+console.log({'q1Count':myQStack.q1Count, 'q1LastEnqueued': myQStack.q1LastEnqueued, 'q2Count':myQStack.q2Count, 'q2LastEnqueued': myQStack.q2LastEnqueued, 'currentQ':myQStack.currentQ});
+
+
+
+console.log('\n ---\n');
+console.log('\n ---\n');
+// write a function that determines if a string has matching braces using a stack
+
+function braceChecker(string) {
+  let strArr = string.split('');
+
+  let myStack = new Stack();
+  let lCounter = 0, rCounter = 0, noBraces = true;
+
+  for(var e of strArr) {
+    if (e === '(') {
+      
+      lCounter++;
+      noBraces = false;
+      myStack.push(e);
+    }
+    else if (e === ')') {
+      rCounter++;
+      noBraces = false;
+      myStack.push(e);
+    }
+    if (rCounter > lCounter)  {
+      return false;
+    }
+    else if (lCounter === rCounter ) {
+      while(myStack & myStack.top ) {
+        myStack.pop();
+      }
+      lCounter = 0;
+      rCounter = 0;
+    }
+  }
+
+  if (noBraces) {return null};
+
+  if (lCounter !== rCounter) { return false}
+
+  return true;
+}
+
+let badStr1 = `(hello))(i must be going)`;
+let badStr2 = `(hello)(i must be going()`;
+let goodStr1 = `(hello)(i must be going)`;
+
+console.log({'checking braces for badStr1':braceChecker(badStr1)});
+console.log({'checking braces for badStr2':braceChecker(badStr2)});
+console.log({'checking braces for goodStr1':braceChecker(goodStr1)});
+
+console.log('\n ---\n');
 // write a function that will remove consecutive dups from a list using a stack
+// NOTE I added code to let the user remove ALL dups if desired
 function consecutiveDupRemover(list, removeAllDups = false){
   let curr = list.head, previous = list.head.value, myStack = new Stack(), filtered = [], noDups = [], output = new LinkedList();
 
--- stacks-and-queues.js
+++ stacks-and-queues.js
@@ -51,7 +51,73 @@ class Stack {
     // console.log('peeking entire stack list: ', this.list.iterator())
     return this.list.tail;
   }
-
 }
 
-module.exports = {Stack, Queue};
+// implement a Stack using queues...
+class QStack {
+  constructor () {
+    this.q1 = new Queue();
+    this.q1Count = 0;
+    this.q1LastEnqueued = null;
+    this.q2 = new Queue();
+    this.q2Count = 0;
+    this.q2LastEnqueued = null;
+    this.currentQ = 1;
+  }
+
+  push(value) {
+    if (this.currentQ === 1) {
+      this.q1.enqueue(value);
+      this.q1LastEnqueued = value;
+      this.q1Count++;
+    } else {
+      this.q2.enqueue(value);
+      this.q2LastEnqueued = value;
+      this.q2Count++;
+    }
+  }
+
+  pop() {
+    let current, popped = null;
+    if (this.currentQ === 1) {
+      while (this.q1Count > 1) {
+        current = this.q1.dequeue().value;
+        this.q2.enqueue(current);
+        this.q1Count--;
+        this.q2Count++;
+        this.q2LastEnqueued = current;
+      }
+      if (this.q1Count === 1) {
+        popped = this.q1.dequeue().value;
+        this.q1Count = 0;
+        this.currentQ = 2;
+        this.q1LastEnqueued = null;
+      }
+    } else {
+      while (this.q2Count > 1) {
+        current = this.q2.dequeue().value;
+        this.q1.enqueue(current);
+        this.q2Count--;
+        this.q1Count++;
+        this.q1LastEnqueued = current;
+      }
+      if (this.q2Count === 1) {
+        popped = this.q2.dequeue().value;
+        this.q2Count = 0;
+        this.currentQ = 1;
+        this.q2LastEnqueued = null;
+      }
+    }
+    return popped;
+  }
+
+  peek() {
+    if (this.currentQ === 1) {
+      return this.q1LastEnqueued;
+    } else {
+      return this.q2LastEnqueued;
+    }
+  }
+}
+
+module.exports = {Stack, Queue, QStack};
