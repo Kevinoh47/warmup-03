b58d014979e1b88895abb79d057c57b1bf508bf1
Kevinoh47 <kohok47@gmail.com>
2019-07-15 13:30:21 -0700

    destructuring assignment practice

--- destructuring.js
+++ destructuring.js
@@ -36,3 +36,38 @@ console.log(state.stuff.cars);
 
 console.log({newState});
 console.log(newState.stuff.cars);
+
+console.log('\n------ more practice spread and destructuring assignment ------\n');
+const band = ['John','Paul','George'];
+
+const gear = {
+  guitar: 'electric',
+  base: 'standup',
+  drums: 'snare',
+  vehicles: ['BandBus','CrewBus']
+}
+
+let bandOperation = {};
+bandOperation = {band: [...band], gear: {...gear}}
+
+let updatedBand = ['Ringo', ...band, 'Joey Ramone'];
+let moreGear = { ...gear, 'vehicles': [...gear.vehicles, 'pressBus']};
+
+console.log({bandOperation});
+console.log('bandOperation vehicles: ', bandOperation.gear.vehicles);
+console.log({updatedBand});
+console.log({moreGear});
+console.log('more gear vehicles: ', moreGear.vehicles);
+
+// add band, with new additions, and gear, with new additions:
+let updatedBandOperation = {};
+updatedBandOperation = 
+  { band: ['Ringo!', ...band, 'Joe Strummer'],
+    gear: {gear: {...gear, vehicles: [...gear.vehicles, 'Austin Healey']}}
+  };
+
+console.log({updatedBandOperation});
+console.log('updatedBandOperation gear: ', updatedBandOperation.gear );
+console.log('updatedBandOperation gear vehicles: ', updatedBandOperation.gear.vehicles );
+
+
